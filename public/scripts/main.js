a2d2d607c607ba4509914b5544ced0a3ef3b4e2b
Hellkiro <hellkiro@yandex.ru>
2021-06-17 20:51:14 +0300

    proto3 with mobile layout

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -19,3 +19,22 @@ function spoilersInit() {
 }
 
 spoilersInit();
+
+function anchorsInit() {
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        
+        const blockID = anchor.getAttribute('href').substr(1)
+        
+        document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+        })
+    })
+    }    
+}
+
+anchorsInit();
